ce41ff10220e7cf26c55c0923febe127e94dccf5
Akshar Ambaliya <137415917+akshar811@users.noreply.github.com>
2024-09-21 05:41:54 +0530

    Update User_controller.js

--- controllers/User_controller.js
+++ controllers/User_controller.js
@@ -1,4 +1,3 @@
-const Task = require("../models/Task_Schema");
 const User = require("../models/User_Schema");
 const jwt = require("jsonwebtoken");
 
@@ -9,23 +8,15 @@ const signups = (req, res) => {
 const loginpage = (req, res) => {
   res.render("login");
 };
-const Userpage = (req, res) => {
-  res.render("User");
-};
-const Adminpage = (req, res) => {
-  res.render("Admin");
-};
 
 const signup = async (req, res) => {
-  const { username, email, password, role } = req.body;
+  const { username, email, password } = req.body;
 
   const users = await User.findOne({ email: email });
   if (users) {
     res.redirect("login");
   } else {
     let newUser = await User.create(req.body);
-
-    res.cookie("role", newUser.role);
     res.cookie("id", newUser.id);
 
     res.redirect("login");
@@ -43,22 +34,15 @@ const login = async (req, res) => {
     return res.send("password not match");
   } else {
     let token = jwt.sign({ id: user._id }, "token");
-    res.cookie("token", token).cookie("id", user._id).cookie("role", user.role);
-    if(user.role=="Admin"){
-        res.redirect("/user/Admin")
-    }
-    else{
-        res.redirect("/user/User")
+    res.cookie("token", token).cookie("id", user._id);
+    if (user.role == "Admin") {
+      res.redirect("/user/Admin");
+    } else {
+      res.redirect("/Event/AllData");
     }
   }
 };
 
-const updateAdminTask = async (req, res) => {
-  let {title,des,category,_id} = req.body;
-
-  let data = await Task.findByIdAndUpdate(_id , req.body);
-  res.redirect("/user/Admin");
-};
 
 
-module.exports = { signups, signup, loginpage, login, Userpage , Adminpage  , updateAdminTask };
+module.exports = { signups, signup, loginpage, login  };
